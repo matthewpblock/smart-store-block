70d90462eb994a528fc29b308faf181af774aa05
matthewpblock <matthewpblock@gmail.com>
2025-03-30 09:04:17 -0500

    customized sql creation scripting

--- scripts/etl_to_dw.sql
+++ scripts/etl_to_dw.sql
@@ -1,22 +1,30 @@
-CREATE TABLE IF NOT EXISTS customer (
+CREATE TABLE IF NOT EXISTS customers (
     customer_id INTEGER PRIMARY KEY,
     name TEXT,
     region TEXT,
-    join_date TEXT
+    join_date TEXT,
+    sex TEXT,
+    age INTEGER,
 );
 
-CREATE TABLE IF NOT EXISTS product (
+CREATE TABLE IF NOT EXISTS products (
     product_id INTEGER PRIMARY KEY,
     product_name TEXT,
-    category TEXT
+    category TEXT,
+    unit_price REAL,
+    wholesale_price REAL,
+    supplier TEXT,
 );
 
-CREATE TABLE IF NOT EXISTS sale (
-    sale_id INTEGER PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS sales (
+    transaction_id INTEGER PRIMARY KEY,
     customer_id INTEGER,
     product_id INTEGER,
     sale_amount REAL,
     sale_date TEXT,
-    FOREIGN KEY (customer_id) REFERENCES customer (customer_id),
-    FOREIGN KEY (product_id) REFERENCES product (product_id)
+    store_id INTEGER,
+    loyalty_points INTEGER,
+    payment_type TEXT,
+    FOREIGN KEY (customer_id) REFERENCES customers (customer_id),
+    FOREIGN KEY (product_id) REFERENCES products (product_id)
 );
